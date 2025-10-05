0d0e2ce1be905b1137b309032f4b8ddd7b09a27c
ariadna-sg <ariadnasanchez15@proton.me>
2025-10-04 22:14:08 -0500

    Add files via upload

---
+++ CRUD_04102025/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CRUD_04102025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Location = new System.Drawing.Point(42, 46);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(133, 36);
+            this.btnConexion.TabIndex = 0;
+            this.btnConexion.Text = "conexión";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(180, 135);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(183, 20);
+            this.txtID.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(180, 172);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(180, 215);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(182, 20);
+            this.txtLName.TabIndex = 6;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(40, 277);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 30);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnMod
+            // 
+            this.btnMod.Location = new System.Drawing.Point(40, 327);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(101, 29);
+            this.btnMod.TabIndex = 8;
+            this.btnMod.Text = "Modificar";
+            this.btnMod.UseVisualStyleBackColor = true;
+            this.btnMod.Click += new System.EventHandler(this.btnMod_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(40, 372);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(100, 28);
+            this.btnDel.TabIndex = 9;
+            this.btnDel.Text = "Eliminar";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConexion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConexion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnMod;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ CRUD_04102025/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+namespace CRUD_04102025
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string CadenaConexion = "Server=localhost;user=root;pasword=;port=3306;database=pruebacensa";
+
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+            PruebaConexion();
+        }
+        public void PruebaConexion()
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(CadenaConexion);
+
+            try
+            {
+                mySqlConnection.Open();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectarse a la DB" + ex.Message);
+                return;
+            }
+            mySqlConnection.Close();
+            MessageBox.Show("Conectado exitosamente a la DB");
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Create();
+        }
+
+        public void Create()
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(CadenaConexion);
+            mySqlConnection.Open();
+            string Query = "INSERT INTO `pruebacensa`.`crud`(`id`,`nombre`,`apellido`) VALUES ('" + txtID.Text + "', '" + txtName.Text + "', '" + txtLName.Text + "' );";
+            MySqlCommand mySqlCommand = new MySqlCommand(Query, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            mySqlConnection.Close();
+            MessageBox.Show("Registro Insertado Exitosamente");
+        }
+
+        private void btnMod_Click(object sender, EventArgs e)
+        {
+            Update_();
+        }
+
+        public void Update_()
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(CadenaConexion);
+            mySqlConnection.Open();
+            string Query = "UPDATE pruebacensa.crud " + "SET nombre = '" + txtName.Text + "', apellido = '" + txtLName.Text + "' " + "WHERE id = '" + txtID.Text + "';";
+            MySqlCommand mySqlCommand = new MySqlCommand(Query, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            mySqlConnection.Close();
+            MessageBox.Show("Registro Actualizado Exitosamente");
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        public void Delete()
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(CadenaConexion);
+            mySqlConnection.Open();
+            string Query = "DELETE FROM `pruebacensa`.`crud`" + "WHERE ID = '" + txtID.Text + "' ";
+            MySqlCommand mySqlCommand = new MySqlCommand(Query, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            mySqlConnection.Close();
+            MessageBox.Show("Registro Eliminado Exitosamente");
+        }
+    }
+}
